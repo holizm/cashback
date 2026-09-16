3cf9bcdf29a87ab7be64639603da57680f9cd655
Saeed Nemati <sn.1361@gmail.com>
2026-09-16 12:04:49 +0330

    committed by script

--- panel/admin/cashbackProgram/list.jsx
+++ panel/admin/cashbackProgram/list.jsx
@@ -10,7 +10,7 @@ const headers = <>
     <th>cashbackCode</th>
     <th>cashbackPercentage</th>
     <th>cashbackEndDate</th>
-    <th>cashbackState</th>
+    <th>stateMachinesState</th>
 </>
 
 const row = item => <>
